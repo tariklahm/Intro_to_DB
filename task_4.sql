10cb1288bc0f99ce49d24aec3dcea30be11ded22
Tarik LAHMAM <tariklahmam91@gmail.com>
2025-12-14 23:53:43 +0100

    Add task4

---
+++ task_4.sql
@@ -0,0 +1,11 @@
+USE alx_book_store;
+SELECT 
+    COLUMN_NAME AS 'Column',
+    COLUMN_TYPE AS 'Type',
+    IS_NULLABLE AS 'Nullable',
+    COLUMN_KEY AS 'Key',
+    COLUMN_DEFAULT AS 'Default',
+    EXTRA AS 'Extra'
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_NAME = 'Books'
+ORDER BY ORDINAL_POSITION;
